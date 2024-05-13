f61a24e07430d4f140a2878c780e407f88f2bd2f
Adrian Pita <pitabermudez@gmail.com>
2024-05-13 17:34:38 +0200

    Leccion 186, finalizar

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroLayoutPageComponent } from './pages/hero-layout-page/hero-layout-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 
 
 //localhost:4200/heroes
@@ -11,9 +14,15 @@ const routes: Routes = [
     component: HeroLayoutPageComponent,
     //Rutas hijas porque van despues del //localhost:4200/heroes/.....
     children: [
-      {
-       path: 'new-hero', component: NewPageComponent
-      }
+      { path: 'new-hero', component: NewPageComponent },
+      { path: 'search', component: SearchPageComponent },
+      { path: 'edit/:id', component: NewPageComponent },
+      { path: 'list', component: ListPageComponent },
+      /*:id es un comodin, tiene que ir al final, si va al principio el resto
+      no entrarian, porque entrarian por :id*/
+      { path: ':id', component: HeroPageComponent },
+      { path: '**', redirectTo: 'list' },
+
     ]
   },
 ];
