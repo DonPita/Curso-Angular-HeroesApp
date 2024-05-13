98e7bcda9861b27142793887fe8dda03011aa328
Adrian Pita <pitabermudez@gmail.com>
2024-05-13 17:25:19 +0200

    Empezando con las rutas, rutas hijas y lazyloads

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthLayoutPageComponent } from './pages/auth-layout-page/auth-layout-page.component';
 
 
-const routes: Routes = [];
+//localhost:4200/auth
+const routes: Routes = [
+  {
+    path: '',
+    component: AuthLayoutPageComponent
+  },
+];
 
 
 @NgModule({
--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroLayoutPageComponent } from './pages/hero-layout-page/hero-layout-page.component';
+import { NewPageComponent } from './pages/new-page/new-page.component';
 
-const routes: Routes = [];
+
+//localhost:4200/heroes
+const routes: Routes = [
+  {
+    path: '',
+    component: HeroLayoutPageComponent,
+    //Rutas hijas porque van despues del //localhost:4200/heroes/.....
+    children: [
+      {
+       path: 'new-hero', component: NewPageComponent
+      }
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
