736eba0e0d60a6010d81c3778d5ada6214ae29e6
Adrian Pita <pitabermudez@gmail.com>
2024-05-15 11:21:34 +0200

    Introducción a los Formularios Reacttivos

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Publisher } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-new-page',
@@ -6,6 +8,18 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class NewPageComponent {
+
+  //Creando el Formulario Reactivo, propiedades son la de la interface
+  public heroForm = new FormGroup({
+    id: new FormControl(''),
+    superhero: new FormControl('', {nonNullable: true}),
+    publisher: new FormControl<Publisher>( Publisher.DCComics),
+    alter_ego: new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters: new FormControl(''),
+    alt_img: new FormControl(''),
+  });
+
 
   //Para en ngFor del creador en el html
   public publishers = [
