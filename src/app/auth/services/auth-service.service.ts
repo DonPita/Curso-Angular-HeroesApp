e9d54c7c2602e9f56ab1bc303d8fcaf7927f1465
Adrian Pita <pitabermudez@gmail.com>
2024-05-15 17:23:31 +0200

    Metodo para comprobar si el usuario esta autentificado. Siguiente clase hay que revisar no se que de los Guards, que sería la 226

--- src/app/auth/services/auth-service.service.ts
+++ src/app/auth/services/auth-service.service.ts
@@ -2,7 +2,7 @@ import { environments } from './../../../environments/environments';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../interfaces/user.interface';
-import { Observable, tap } from 'rxjs';
+import { Observable, catchError, map, of, tap } from 'rxjs';
 
 
 @Injectable({ providedIn: 'root' })
@@ -29,6 +29,26 @@ export class AuthService {
         tap(user => this.user = user),
         tap(user => localStorage.setItem('token', user.id.toString())),
       )
+
+  }
+
+
+  checkAuthentication(): Observable<boolean> | boolean {
+
+    if (!localStorage.getItem('token')) {
+      return false;
+    }
+
+    const token = localStorage.getItem('token');
+
+    return this.httpClient.get<User>(`${this.baseUrl}/users/1`)
+      .pipe(
+        tap(user => this.user = user),
+        /*Metemos la doble negacion en user !!, porque lo queremos transformar en
+        un valor booleano, porque es lo que retorna la función.*/
+        map(user => !!user),
+        catchError(err => of(false))
+      );
 
   }
 
